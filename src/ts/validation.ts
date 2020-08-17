273633d0ac38e9084cdc67b670454038a1107e21
Carlos Dubón <carlos.dubon@outlook.es>
2020-08-17 08:13:39 -0600

    Update validation.ts
    Check for the pawn basic movement, still not eating

--- src/ts/validation.ts
+++ src/ts/validation.ts
@@ -11,9 +11,22 @@ export interface Tile {
 
 export default function validate(start: Tile, end: Tile) {
   if (start.piece == CONSTANTS.wP || start.piece == CONSTANTS.bP) {
+    //A pawn was selected
     if (end.piece == "none") {
+      //If the pawn is just moving
       if (start.x == 6 || start.x == 1) {
-        //Check if the pawn hasn't moved, if not it has 2 available moves
+        //Check if the pawn hasn't moved, if not: it has 2 available moves
+        if (
+          getVectorComponents(start, end)[0] <= 2 &&
+          getVectorComponents(start, end)[1] == 0
+        ) {
+          //This is an available move for the pawn
+        }
+      } else {
+        //The pawn only has 1 upward move available
+        if(getVectorComponents(start, end)[0] <=1 && getVectorComponents(start, end)[1]==0{
+          //This is an available move for the pawn
+        }
       }
     }
   } else if (start.piece == CONSTANTS.wR || start.piece == CONSTANTS.bR) {
