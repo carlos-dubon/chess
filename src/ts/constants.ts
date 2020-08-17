54eaf1ac66b6bbdde1a13da1cf6c29f71688858b
Carlos Dubón <carlos.dubon@outlook.es>
2020-08-16 20:22:44 -0600

    Updated constants

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -2,18 +2,16 @@ export const board: Element | null = document.querySelector(".board");
 export const boardRows = 8;
 export const boardCols = 8;
 
-//White's
-export const wK: string = "k"; //King
-export const wQ: string = "q"; //Queen
-export const wR: string = "r"; //Rook
-export const wB: string = "b"; //Bishop
-export const wH: string = "h"; //Horse
-export const wP: string = "p"; //Pawn
+export const wK: string = "k"; //White king
+export const wQ: string = "q"; //White queen
+export const wR: string = "r"; //White rook
+export const wB: string = "b"; //White bishop
+export const wH: string = "h"; //White horse
+export const wP: string = "p"; //White pawn
 
-//Black's
-export const bK: string = "l"; //King
-export const bQ: string = "w"; //Queen
-export const bR: string = "t"; //Rook
-export const bB: string = "n"; //Bishop
-export const bH: string = "j"; //Horse
-export const bP: string = "o"; //Pawn
+export const bK: string = "l"; //Black king
+export const bQ: string = "w"; //Black queen
+export const bR: string = "t"; //Black rook
+export const bB: string = "n"; //Black bishop
+export const bH: string = "j"; //Black horse
+export const bP: string = "o"; //Black pawn
--- src/ts/validation.ts
+++ src/ts/validation.ts
@@ -8,10 +8,6 @@ interface Tile {
 }
 
 export default function validate(start: Tile, end: Tile) {
-  console.log(`1. ${start.piece} was the start piece.`);
-  console.log(`2. ${end.piece} was the end piece.`);
-  console.log(`The value of the wP constant is ${CONSTANTS.wP}`);
-
   if (start.piece == CONSTANTS.wP) {
     console.log("White Pawn selected");
   }
