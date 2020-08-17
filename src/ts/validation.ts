d75225bfcea3d9bec26e3c702687c49cf24757ee
Carlos Dubón <carlos.dubon@outlook.es>
2020-08-17 09:17:02 -0600

    Rook validation implemented

--- src/ts/validation.ts
+++ src/ts/validation.ts
@@ -13,7 +13,7 @@ export default function validate(start: Tile, end: Tile) {
   if (start.piece == CONSTANTS.wP || start.piece == CONSTANTS.bP) {
     //A pawn was selected
     if (end.piece == "none") {
-      //If the pawn is just moving
+      //The pawn is just moving
       if (start.x == 6 || start.x == 1) {
         //Check if the pawn hasn't moved, if not: it has 2 available moves
         if (
@@ -24,13 +24,27 @@ export default function validate(start: Tile, end: Tile) {
         }
       } else {
         //The pawn only has 1 upward move available
-        if(getVectorComponents(start, end)[0] <=1 && getVectorComponents(start, end)[1]==0){
+        if (
+          getVectorComponents(start, end)[0] <= 1 &&
+          getVectorComponents(start, end)[1] == 0
+        ) {
           //This is an available move for the pawn
         }
       }
     }
   } else if (start.piece == CONSTANTS.wR || start.piece == CONSTANTS.bR) {
     //A rook was selected
+    if (end.piece == "none") {
+      //The rook is just moving
+      if (
+        (getVectorComponents(start, end)[0] <= 7 &&
+          getVectorComponents(start, end)[1] == 0) ||
+        (getVectorComponents(start, end)[0] == 0 &&
+          getVectorComponents(start, end)[1] <= 7)
+      ) {
+        //This is an available move for the rook
+      }
+    }
   } else if (start.piece == CONSTANTS.wH || start.piece == CONSTANTS.bH) {
     //A knight was selected
   } else if (start.piece == CONSTANTS.wB || start.piece == CONSTANTS.bB) {
