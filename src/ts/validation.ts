eb99d33af13da3245097f0cedd7ce3cd0362346e
Carlos Dubón <carlos.dubon@outlook.es>
2020-08-17 17:48:11 -0600

    Update validation.ts
    Added the knight movement validation
    -Fixed the pawns validation

--- src/ts/validation.ts
+++ src/ts/validation.ts
@@ -10,26 +10,40 @@ export interface Tile {
 }
 
 export default function validate(start: Tile, end: Tile) {
+  const yDistance = getVectorComponents(start, end)[0];
+  const xDistance = getVectorComponents(start, end)[1];
+
   if (start.piece == CONSTANTS.wP || start.piece == CONSTANTS.bP) {
     //A pawn was selected
     if (end.piece == "none") {
       //The pawn is just moving
       if (start.x == 6 || start.x == 1) {
         //Check if the pawn hasn't moved, if not: it has 2 available moves
-        if (
-          getVectorComponents(start, end)[0] <= 2 &&
-          getVectorComponents(start, end)[1] == 0
-        ) {
+        if (yDistance <= 2 && xDistance == 0) {
           //This is an available move for the pawn
-          console.log("Hello")
+          if (start.piece == CONSTANTS.wP) {
+            if (end.x < start.x) {
+              //This is a valid movement for the whites
+            }
+          } else {
+            if (end.x > start.x) {
+              //This is a valid movement for the blacks
+            }
+          }
         }
       } else {
         //The pawn only has 1 upward move available
-        if (
-          getVectorComponents(start, end)[0] <= 1 &&
-          getVectorComponents(start, end)[1] == 0
-        ) {
+        if (yDistance <= 1 && xDistance == 0) {
           //This is an available move for the pawn
+          if (start.piece == CONSTANTS.wP) {
+            if (end.x < start.x) {
+              //This is a valid movement for the whites
+            }
+          } else {
+            if (end.x > start.x) {
+              //This is a valid movement for the blacks
+            }
+          }
         }
       }
     }
@@ -38,16 +52,23 @@ export default function validate(start: Tile, end: Tile) {
     if (end.piece == "none") {
       //The rook is just moving
       if (
-        (getVectorComponents(start, end)[0] <= 7 &&
-          getVectorComponents(start, end)[1] == 0) ||
-        (getVectorComponents(start, end)[0] == 0 &&
-          getVectorComponents(start, end)[1] <= 7)
+        (yDistance <= 7 && xDistance == 0) ||
+        (yDistance == 0 && xDistance <= 7)
       ) {
         //This is an available move for the rook
       }
     }
   } else if (start.piece == CONSTANTS.wH || start.piece == CONSTANTS.bH) {
     //A knight was selected
+    if (end.piece == "none") {
+      //The knight is just moving
+      if (
+        (yDistance == 2 && xDistance == 1) ||
+        (yDistance == 1 && xDistance == 2)
+      ) {
+        console.log("The knight can move here!");
+      }
+    }
   } else if (start.piece == CONSTANTS.wB || start.piece == CONSTANTS.bB) {
     //A bishop was selected
   } else if (start.piece == CONSTANTS.wQ || start.piece == CONSTANTS.bQ) {
